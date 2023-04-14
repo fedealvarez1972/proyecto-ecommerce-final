ea7c76cef4357a4a9a4c7c8120addd43b536518e
Federico Alvarez <fedealv1972@gmail.com>
2023-04-13 23:40:21 -0300

    delete file

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import ProductsDetail from './Page/ProductsDetail'
 import Login from './Page/Login'
 import Purchases from './Page/Purchases'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
 
 
 function App() {
