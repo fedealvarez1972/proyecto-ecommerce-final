27e8765688f069ca860fc5821668193d96a69109
Federico Alvarez <fedealv1972@gmail.com>
2023-04-13 22:47:48 -0300

    second commit

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import products from "./slices/Products.slice";
+import Products from "./slices/Products.slice"
 import IsLoading from "./slices/IsLoading.slice";
 
 
 export default configureStore({
   reducer: {
-    products, IsLoading
+    products : Products, 
+    IsLoading
   }
 })
